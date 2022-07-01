f1539409a7d02337655b677142f14ae61de404b1
Maneesh <maneesh888@gmail.com>
2022-07-01 13:35:50 +0400

    Button Actions added

--- flutter_learning/lib/main.dart
+++ flutter_learning/lib/main.dart
@@ -1,13 +1,44 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
+    var questionIndex = 0;
+    void answerQuestion() {
+      questionIndex = questionIndex + 1;
+      print(questionIndex);
+    }
+
+    var questions = [
+      'What\'s your favorate color',
+      'What\'s your favourate animal'
+    ];
     return MaterialApp(
-      home: Text('Helloww!'),
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('My First App'),
+          ),
+          body: Column(
+            children: [
+              Text(questions.elementAt(questionIndex)),
+              RaisedButton(
+                child: Text('Answer 1'),
+                onPressed: answerQuestion,
+              ),
+              RaisedButton(
+                child: Text('Answer 2'),
+                onPressed: answerQuestion,
+              ),
+              RaisedButton(
+                child: Text('Answer 3'),
+                onPressed: () {
+                  print('Answer 3 choosen');
+                },
+              )
+            ],
+          )),
     );
   }
 }
